1ce8f64d87a81d77ee95e9fc9137aca70b54d781
Suphakit_Dechudomchot <fiw.suphakit@gmail.com>
2025-09-15 19:46:33 +0700

    voting but cant use

--- kemo/Scenes/voting.gd
+++ kemo/Scenes/voting.gd
@@ -1,58 +1,257 @@
+# Voting.gd
 extends Control
 
-@onready var timer_label = $TimerLabel
-@onready var player_grid = $PlayerGrid
-@onready var skip_button = $SkipButton
-@onready var leader_banner = $LeaderBanner/Label
+var turn_duration := 60
+var time_left := 0
 
-var PlayerCard = preload("res://Scenes/PlayerCard.tscn")
-var voting_time := 180  # 3 minutes in seconds
+@onready var Voting_topic = $status
+@onready var Voting_results = $"Vote results"
+
+@onready var turn_timer: Timer = $Timer
+@onready var timer_label: Label = $Time
+
+# Player UI Nodes
+@onready var player_ui_nodes = {
+    1: $Player/player1, 2: $Player/player2, 3: $Player/player3,
+    4: $Player/player4, 5: $Player/player5, 6: $Player/player6,
+    7: $Player/player7, 8: $Player/player8, 9: $Player/player9,
+    10: $Player/player10
+}
+@onready var player_name_labels = {
+    1: $Player/player1/player1_name, 2: $Player/player2/player2_name, 3: $Player/player3/player3_name,
+    4: $Player/player4/player4_name, 5: $Player/player5/player5_name, 6: $Player/player6/player6_name,
+    7: $Player/player7/player7_name, 8: $Player/player8/player8_name, 9: $Player/player9/player9_name,
+    10: $Player/player10/player10_name
+}
+@onready var player_modulate_nodes = {
+    1: $Player/player1/Modulate, 2: $Player/player2/Modulate, 3: $Player/player3/Modulate,
+    4: $Player/player4/Modulate, 5: $Player/player5/Modulate, 6: $Player/player6/Modulate,
+    7: $Player/player7/Modulate, 8: $Player/player8/Modulate, 9: $Player/player9/Modulate,
+    10: $Player/player10/Modulate
+}
+
+# Dictionary for the vote status icon
+@onready var Selected = {
+    1: $Player/player1/Selected, 2: $Player/player2/Selected, 3: $Player/player3/Selected,
+    4: $Player/player4/Selected, 5: $Player/player5/Selected, 6: $Player/player6/Selected,
+    7: $Player/player7/Selected, 8: $Player/player8/Selected, 9: $Player/player9/Selected,
+    10: $Player/player10/Selected
+}
+
+@onready var Select_panel = $Select_panel
+@onready var Select_button = $Select_panel/Select
+@onready var Cancel_button = $Select_panel/Cancel
+@onready var skip_button = $Skip_button
+
+var votes: Dictionary = {}
+var has_voted := false
+var selected_player_id: int = 0
+var eliminated_player_id: int = 0
+var current_highlighted_node: Node = null
+
+var player_id_to_ui_index: Dictionary = {}
+
 
 func _ready():
-	print("Voting scene loaded")
-	
-	# Show leader if assigned
-	var leader_id = Global.special_roles["Leader"]
-	if leader_id != null:
-		leader_banner.text = "Leader: " + Global.player_names[leader_id]
-	else:
-		leader_banner.text = "No Leader Assigned"
+    for ui in player_ui_nodes.values():
+        ui.visible = false
+        
+    hide_all_selected()
+        
+    Select_button.pressed.connect(_on_select_button_pressed)
+    Cancel_button.pressed.connect(_on_cancel_button_pressed)
+    skip_button.pressed.connect(_on_skip_button_pressed)
+    
+    Select_panel.visible = false
+    setup_player_ui()
 
-	# Build player cards
-	for peer_id in Global.player_names.keys():
-		var card = PlayerCard.instantiate()
-		card.setup(peer_id, Global.player_names[peer_id], "?")  # cleaner setup call
-		player_grid.add_child(card)
+    if multiplayer.is_server():
+        start_voting_timer()
 
-	# connect the vote button
-		card.vote_button.pressed.connect(
-		func():
-			_on_vote_pressed(peer_id)
-	)
 
-	skip_button.pressed.connect(_on_skip_pressed)
+func hide_all_selected():
+    for icon in Selected.values():
+        icon.visible = false
+        
+func highlight_selection(player_id: int):
+    if current_highlighted_node:
+        current_highlighted_node.modulate = Color.WHITE
+        
+    var ui_index = player_id_to_ui_index.get(player_id)
+    if ui_index != null:
+        var name_label = player_name_labels.get(ui_index)
+        if name_label:
+            name_label.modulate = Color.YELLOW
+            current_highlighted_node = name_label
 
-	# Start countdown
-	set_process(true)
+func _on_player_ui_pressed(player_id: int):
+    if not has_voted:
+        selected_player_id = player_id
+        highlight_selection(player_id)
+        Select_panel.visible = true
 
-func _process(delta):
-	if voting_time > 0:
-		voting_time -= delta
-		var minutes = int(voting_time) / 60
-		var seconds = int(voting_time) % 60
-		timer_label.text = str(minutes) + ":" + str(seconds).pad_zeros(2)
-	else:
-		_end_voting()
+func _on_select_button_pressed():
+    if selected_player_id != 0:
+        rpc("send_vote", selected_player_id)
+        Select_panel.visible = false
+        has_voted = true
+        Voting_topic.text = "You have voted!"
 
-func _on_vote_pressed(peer_id):
-	print("Voted for", Global.player_names[peer_id])
-	# TODO: send vote to server / GameManager
+func _on_cancel_button_pressed():
+    Select_panel.visible = false
+    if current_highlighted_node:
+        current_highlighted_node.modulate = Color.WHITE
+    selected_player_id = 0
 
-func _on_skip_pressed():
-	print("Skip vote pressed")
-	# TODO: send skip action to server
+func _on_skip_button_pressed():
+    if not has_voted:
+        rpc("send_vote", 0)
+        has_voted = true
+        Voting_topic.text = "You have skipped your vote."
+        skip_button.disabled = true
+        Select_panel.visible = false
+        if current_highlighted_node:
+            current_highlighted_node.modulate = Color.WHITE
+        selected_player_id = 0
 
-func _end_voting():
-	print("Voting ended")
-	# TODO: tally votes and change scene
-	get_tree().change_scene_to_file("res://Scenes/YOUR_NEXT_SCENE.tscn")
+func setup_player_ui():
+    var active_players = Global.player_names.keys()
+    
+    for i in range(active_players.size()):
+        var peer_id = active_players[i]
+        var ui_index = i + 1
+        
+        player_id_to_ui_index[peer_id] = ui_index
+        
+        var ui_node = player_ui_nodes.get(ui_index)
+        var name_label = player_name_labels.get(ui_index)
+        var modulate_node = player_modulate_nodes.get(ui_index)
+        
+        if ui_node and name_label and modulate_node:
+            ui_node.visible = true
+            name_label.text = Global.player_names[peer_id]
+            modulate_node.modulate = Global.player_colors[peer_id]
+            
+            ui_node.gui_input.connect(func(event):
+                if event is InputEventMouseButton and event.pressed:
+                    if event.button_index == MOUSE_BUTTON_LEFT:
+                        _on_player_ui_pressed(peer_id)
+            )
+
+# ----------------------------------------------------
+# Multiplayer RPC functions (Server Logic)
+# ----------------------------------------------------
+@rpc("authority")
+func send_vote(voted_id: int):
+    var voter_id = multiplayer.get_remote_sender_id()
+    if not votes.has(voter_id):
+        votes[voter_id] = voted_id
+        print("Player %s voted for Player %s" % [voter_id, voted_id])
+        
+        rpc("show_selected_rpc", voter_id)
+        
+    if votes.size() >= Global.player_names.size():
+        turn_timer.stop()
+        calculate_and_show_results()
+
+func start_voting_timer():
+    if not multiplayer.is_server():
+        return
+    
+    time_left = turn_duration
+    turn_timer.wait_time = 1.0
+    turn_timer.one_shot = false
+    turn_timer.timeout.connect(_on_timer_timeout)
+    turn_timer.start()
+    
+    rpc("update_timer_label", time_left)
+    
+func _on_timer_timeout():
+    if multiplayer.is_server():
+        time_left -= 1
+        if time_left < 0:
+            turn_timer.stop()
+            calculate_and_show_results()
+        else:
+            rpc("update_timer_label", time_left)
+
+func calculate_and_show_results():
+    var vote_counts: Dictionary = {}
+    var skip_count = 0
+    
+    for voter_id in votes.keys():
+        var voted_id = votes[voter_id]
+        if voted_id == 0:
+            skip_count += 1
+            continue
+            
+        if vote_counts.has(voted_id):
+            vote_counts[voted_id] += 1
+        else:
+            vote_counts[voted_id] = 1
+
+    var max_votes = 0
+    var winning_voted_id: int = 0
+    var tied_players: Array = []
+
+    for voted_id in vote_counts.keys():
+        var current_votes = vote_counts[voted_id]
+        if current_votes > max_votes:
+            max_votes = current_votes
+            winning_voted_id = voted_id
+            tied_players.clear()
+            tied_players.append(voted_id)
+        elif current_votes == max_votes:
+            tied_players.append(voted_id)
+    
+    var result_text = ""
+    
+    print("--- Vote results on server ---")
+    print("Vote Counts:", vote_counts)
+    print("Skip Votes:", skip_count)
+    print("Max Votes:", max_votes)
+
+    if max_votes == 0:
+        result_text = "No one was voted out."
+    elif tied_players.size() > 1:
+        result_text = "Tied vote. No one was eliminated."
+    elif max_votes <= skip_count:
+        result_text = "Skipped votes were higher than or equal to any single player's vote count. No one was eliminated."
+    else:
+        var eliminated_player_name = Global.player_names.get(winning_voted_id, "Unknown Player")
+        result_text = "\"%s\" was eliminated from the mission." % eliminated_player_name
+
+    rpc("show_final_result", result_text)
+    
+# ----------------------------------------------------
+# Multiplayer RPC functions (Client & Server Logic)
+# ----------------------------------------------------
+@rpc("any_peer")
+func show_selected_rpc(voter_id: int):
+    var ui_index = player_id_to_ui_index.get(voter_id)
+    if ui_index != null:
+        var selected_icon = Selected.get(ui_index)
+        if selected_icon:
+            selected_icon.visible = true
+            
+@rpc("any_peer", "call_local")
+func update_timer_label(new_time: int):
+    time_left = new_time
+    timer_label.text = str(time_left)
+
+@rpc("any_peer", "call_local")
+func show_final_result(result_text: String):
+    Voting_results.text = result_text
+    
+    for ui in player_ui_nodes.values():
+        ui.visible = false
+    
+    if current_highlighted_node:
+        current_highlighted_node.modulate = Color.WHITE
+        
+    hide_all_selected()
+    
+    skip_button.visible = false
+    
+    await get_tree().create_timer(5.0).timeout
+    get_tree().change_scene_to_file("res://Scenes/game.tscn")
--- kemo/Scripts/GameManager.gd
+++ kemo/Scripts/GameManager.gd
@@ -5,101 +5,101 @@ var player_roles = {}
 var roles := ["Data Retriever", "Support", "The Oracle", "Tracer", "Enforcer", "Hacker", "System Controller"]
 # NEW: store special roles
 var special_roles := {
-	"Leader": null
+    "Leader": null
 }
 
 @rpc("any_peer", "reliable", "call_local")
 func start_game():
-	print("Changing scene to game...")
-	if get_tree():
-		get_tree().change_scene_to_file("res://Scenes/game.tscn")
+    print("Changing scene to game...")
+    if get_tree():
+        get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
-		if multiplayer.is_server():
-			await get_tree().create_timer(0.2).timeout
-			assign_roles()
+        if multiplayer.is_server():
+            await get_tree().create_timer(0.2).timeout
+            assign_roles()
 
 func on_role_reveal_finished():
-	if multiplayer.is_server():
-		print("Role reveal finished. Starting game timer...")
-		var game_node = get_tree().get_current_scene()
-		if game_node.has_method("start_turn_timer"):
-			game_node.start_turn_timer()
+    if multiplayer.is_server():
+        print("Role reveal finished. Starting game timer...")
+        var game_node = get_tree().get_current_scene()
+        if game_node.has_method("start_turn_timer"):
+            game_node.start_turn_timer()
 
 
 # Predefined roles
 
 func _ready():
-	if multiplayer.is_server():
-		print("GameManager ready on server.")
+    if multiplayer.is_server():
+        print("GameManager ready on server.")
 
 
 func assign_roles():
-	if not multiplayer.is_server():
-		return
+    if not multiplayer.is_server():
+        return
 
-	var connected_players = multiplayer.get_peers()
-	connected_players.append(multiplayer.get_unique_id())
+    var connected_players = multiplayer.get_peers()
+    connected_players.append(multiplayer.get_unique_id())
 
-	player_roles.clear()
+    player_roles.clear()
 
-	# Build role pool based on counts
-	var role_pool: Array = []
-	for role in Global.role_counts.keys():
-		for i in range(Global.role_counts[role]):
-			role_pool.append(role)
+    # Build role pool based on counts
+    var role_pool: Array = []
+    for role in Global.role_counts.keys():
+        for i in range(Global.role_counts[role]):
+            role_pool.append(role)
 
-	role_pool.shuffle()
+    role_pool.shuffle()
 
-	for id in connected_players:
-		var role = role_pool.pop_front() if role_pool.size() > 0 else "Crewmate"
-		player_roles[id] = role
+    for id in connected_players:
+        var role = role_pool.pop_front() if role_pool.size() > 0 else "Crewmate"
+        player_roles[id] = role
 
-		var player_node = get_player_by_id(id)
-		if player_node:
-			# 👇 Tell only that peer what their role is
-			player_node.rpc_id(id, "set_role", role)
+        var player_node = get_player_by_id(id)
+        if player_node:
+            # 👇 Tell only that peer what their role is
+            player_node.rpc_id(id, "set_role", role)
 
-		print("Assigning role", role, "to peer", id)
+        print("Assigning role", role, "to peer", id)
 
 
 
 
 func get_player_by_id(id: int) -> Node:
-	for player in get_tree().get_nodes_in_group("players"):
-		if player.get_multiplayer_authority() == id:
-			return player
-	return null
+    for player in get_tree().get_nodes_in_group("players"):
+        if player.get_multiplayer_authority() == id:
+            return player
+    return null
 
 
 # ✅ NEW: assign Leader role when voting starts
 func assign_leader():
-	if not multiplayer.is_server():
-		return
+    if not multiplayer.is_server():
+        return
 
-	var all_players = player_roles.keys()
-	if all_players.size() == 0:
-		return
+    var all_players = player_roles.keys()
+    if all_players.size() == 0:
+        return
 
-	var random_player = all_players[randi() % all_players.size()]
+    var random_player = all_players[randi() % all_players.size()]
 
-	# Replace old role with Leader
-	player_roles[random_player] = "Leader"
-	special_roles["Leader"] = random_player
+    # Replace old role with Leader
+    player_roles[random_player] = "Leader"
+    special_roles["Leader"] = random_player
 
-	print("Leader chosen:", random_player)
+    print("Leader chosen:", random_player)
 
-	# Tell only that peer that their role changed
-	var player_node = get_player_by_id(random_player)
-	if player_node:
-		player_node.rpc_id(random_player, "set_role", "Leader")
+    # Tell only that peer that their role changed
+    var player_node = get_player_by_id(random_player)
+    if player_node:
+        player_node.rpc_id(random_player, "set_role", "Leader")
 
-	# (Optional) notify others that this player’s role changed silently
-	# e.g., for debugging/logging
+    # (Optional) notify others that this player’s role changed silently
+    # e.g., for debugging/logging
 
 
 @rpc("authority", "call_local", "reliable")
 func set_leader(peer_id: int):
-	special_roles["Leader"] = peer_id
-	var player_node = get_player_by_id(peer_id)
-	if player_node:
-		player_node.set_leader_local()
+    special_roles["Leader"] = peer_id
+    var player_node = get_player_by_id(peer_id)
+    if player_node:
+        player_node.set_leader_local()
